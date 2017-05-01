438528cd3c3a36cf98822e3ff8edc91b1d78cf7e
nadiaaamand <nadiaaamand@Nadias-MacBook-Pro.local>
2017-05-01 14:50:13 +0200

    Playting with the login

--- userpage.php
+++ userpage.php
@@ -31,52 +31,25 @@
 </head>
 
 <body>
-
- <?php 
-	include 'navigation.php';
-?>
-
-    <!-- Page Content -->
-    <div class="container space">
-
-        <!-- Page Heading/Breadcrumbs -->
-        <div class="row">
-            <div class="col-lg-12">
-                <h1 class="page-header">Login
-				</h1>
-            </div>
-        </div>
-        <!-- /.row -->
-
-        <!-- Content Row -->
-        <div class="row">
-            
-            <!-- Contact Details Column -->
-            <div class="col-md-4">
-                <p></p>
-            </div>
-
-       
-        </div>
-        <!-- /.row -->
-	</div>
-
-        <?php 
-	include 'footer.php';
-?>
-    <!-- /.container -->
-
-    <!-- jQuery -->
-    <script src="js/jquery.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
-
-    <!-- Contact Form JavaScript -->
-    <!-- Do not edit these files! In order to set the email address and subject line for the contact form go to the bin/contact_me.php file. -->
-    <script src="js/jqBootstrapValidation.js"></script>
-    <script src="js/contact_me.js"></script>
-
+<div class="row">
+<div class="col-lg-6">
+			<?php
+			if (isset($_SESSION['id'])){
+				echo "<form action='logout.php'>
+						<p>You are already logged in! <br> You can log out by pressing the log out button.</p>
+						<button>LOG OUT</button>
+						</form>";
+			} else {
+				echo "<form id='signupform' action='login.php' method='POST'>
+						<input type='username' name='username' placeholder='Username'><br>
+						<input type='password' name='pwd' placeholder='Password'><br>
+						<button type='submit'>LOGIN</button>
+					</form>";
+			}
+			?>
+		
+		</div>
+</div>
 </body>
 
 </html>
